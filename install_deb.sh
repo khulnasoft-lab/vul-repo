5c637cf4348443748fecf70085413c0871061fac
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-20 07:47:27 +0600

    Create install_deb.sh

---
+++ install_deb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+set -o xtrace
+
+# Install prerequisites
+apt-get update
+apt-get install -y wget gnupg lsb-release
+
+
+# Install script
+apt-get install -y apt-transport-https
+wget -qO - https://khulnasoft-lab.github.io/vul-repo/deb/public.key | apt-key add -
+echo deb https://khulnasoft-lab.github.io/vul-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list
+apt-get update
+apt-get install -y vul
