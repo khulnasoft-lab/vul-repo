b76d059ee666dae3563cc167c0ebd81c77934837
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-20 08:25:30 +0600

    Update install_deb.sh

--- install_deb.sh
+++ install_deb.sh
@@ -12,6 +12,7 @@ apt-get install -y wget gnupg lsb-release
 
 # Install script
 apt-get install -y apt-transport-https
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E9D0A3616276FA6C
 echo deb https://khulnasoft-lab.github.io/vul-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list
 apt-get update
 apt-get install -y vul
